3a9bc1891571d17f68af2e5508c018ecedb5b29d
Bora <bora_atik98@hotmail.com>
2025-01-28 14:58:13 +0300

    ci

--- src/app/utils/getProvier.js
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-
-export function getProvider() {
-  const provider = new ethers.JsonRpcProvider(
-    "https://eth-sepolia.public.blastapi.io"
-  );
-  return provider;
-}
--- src/app/utils/toHex.js
+++
@@ -1,7 +0,0 @@
-export function toHex(value) {
-  if (typeof value !== "number" && typeof value !== "bigint") {
-    throw new Error("Input must be a number or bigint.");
-  }
-  // Convert the number or bigint to a hex string and add the "0x" prefix
-  return `0x${value.toString(16)}`;
-}
